c8cefc16ee78b1c9cc72e416b9f97da693417e6a
Vasyl Kozhushko <kozhushko.vasyl@gmail.com>
2017-07-13 22:05:49 +0300

    v0.5
    - add some design
    - beautify code

--- main.js
+++ main.js
@@ -4,10 +4,10 @@ var objData;
 var date = new Date();
 var week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-if (date.getDay()+2 > 6) {
+if (date.getDay() + 2 > 6) {
     document.getElementById("dayThreeName").innerHTML = week[date.getDay() - 5];
 } else {
-    document.getElementById("dayThreeName").innerHTML = week[date.getDay()+2];
+    document.getElementById("dayThreeName").innerHTML = week[date.getDay() + 2];
 }
 
 //==========================
@@ -21,19 +21,20 @@ function putData() {
     }
     document.getElementById("city").innerHTML = objData.location.name;
     document.getElementById("currentImg").innerHTML = '<img src = "./img/' + objData.current.condition.code + '.png" id = "currentImg">';
-    document.getElementById("currentImg").setAttribute("alt" , objData.current.condition.text);
+    document.getElementById("currentImg").setAttribute("alt", objData.current.condition.text);
     document.getElementById("currentTemp").innerHTML = curDegree;
+    document.getElementById("currentSunUp").innerHTML = objData.forecast.forecastday[0].astro.sunrise;
+    document.getElementById("currentSunDown").innerHTML = objData.forecast.forecastday[0].astro.sunset;
 
     for (var i = 0; i < 3; i++) {
         var img = "day" + i + "Img";
-        // var cond = "day" + i + "con";
         var temp = "day" + i + "Temp";
         var hum = "day" + i + "Hum";
         var pres = "day" + i + "Pres";
         var wind = "day" + i + "Wind";
         var cloud = "day" + i + "Cloud";
         var degree = Math.round(objData.forecast.forecastday[i].day.avgtemp_c);
-        var altImg = "dayImg"+i;
+        var altImg = "dayImg" + i;
 
         //==========================
         // Switch Celsius to Fahrenheit
@@ -44,12 +45,11 @@ function putData() {
         }
         // =========================
 
-        document.getElementById(img).innerHTML = '<img src = "./img/' + objData.forecast.forecastday[i].day.condition.code + '.png" id="' + altImg +'">';
-        document.getElementById(altImg).setAttribute("alt" , objData.forecast.forecastday[i].day.condition.text);
-        // document.getElementById(cond).innerHTML = objData.forecast.forecastday[i].day.condition.text;
+        document.getElementById(img).innerHTML = '<img src = "./img/' + objData.forecast.forecastday[i].day.condition.code + '.png" id="' + altImg + '">';
+        document.getElementById(altImg).setAttribute("alt", objData.forecast.forecastday[i].day.condition.text);
         document.getElementById(temp).innerHTML = degree;
         document.getElementById(hum).innerHTML = Math.round(objData.forecast.forecastday[i].day.avghumidity) + "%";
-        document.getElementById(wind).innerHTML = Math.round(objData.forecast.forecastday[i].day.maxwind_kph * 1000/3600) + " m/s";
+        document.getElementById(wind).innerHTML = Math.round(objData.forecast.forecastday[i].day.maxwind_kph * 1000 / 3600) + " m/s";
         document.getElementById(cloud).innerHTML = objData.forecast.forecastday[i].hour[0].cloud + "%";
     }
 }
@@ -77,7 +77,6 @@ function success(pos) {
             $.ajax({
                 url: "https://api.apixu.com/v1/forecast.json?key=32b5b567c77b4a20aa5161100170807&q=" + lat + "," + long + "&days=3",
                 success: function(result) {
-                    console.log(result);
                     objData = result;
                     putData();
                 }
@@ -89,7 +88,7 @@ function success(pos) {
 
 function error(err) {
     // document.getElementById("block").innerHTML = "ERROR " + err.code + ":" + err.message;
-    console.warn("ERROR " + err.code + " "+ err.message);
+    console.warn("ERROR " + err.code + " " + err.message);
 }
 
 navigator.geolocation.getCurrentPosition(success, error, options);
@@ -97,19 +96,18 @@ navigator.geolocation.getCurrentPosition(success, error, options);
 //==========================
 // Submit form to get weather;
 //==========================
-function inputVal(){
-  if(document.getElementById("input").value){
-    $.ajax({
-        url: "https://api.apixu.com/v1/forecast.json?key=32b5b567c77b4a20aa5161100170807&q=" + document.getElementById("input").value + "&days=3",
-        success: function(result) {
-            console.log(result);
-            document.getElementById("warning").innerHTML = "";
-            objData = result;
-            putData();
-        },
-        error: function(result){
-          document.getElementById("warning").innerHTML = "Please write correct city name";
-        }
-    });
-  }
+function inputVal() {
+    if (document.getElementById("input").value) {
+        $.ajax({
+            url: "https://api.apixu.com/v1/forecast.json?key=32b5b567c77b4a20aa5161100170807&q=" + document.getElementById("input").value + "&days=3",
+            success: function(result) {
+                document.getElementById("warning").innerHTML = "";
+                objData = result;
+                putData();
+            },
+            error: function(result) {
+                document.getElementById("warning").innerHTML = "Please write correct city name";
+            }
+        });
+    }
 }
